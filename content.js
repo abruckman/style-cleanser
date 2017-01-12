8d4c24759fdedcb5276b28e6091bd96d4e1e7382
Andrew Bruckman <abruckman09@gmail.com>
2017-01-11 16:01:41 -0800

    color and background change

--- content.js
+++ content.js
@@ -1,8 +1,10 @@
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript({
-    code: 'window.getSelection().anchorNode.parentElement.stlye.cssText = "background-color: ";'
-  })
+    file: 'color-change.js',
+  //   code: 'window.getSelection().anchorNode.parentElement.stlye.cssText = "background-color: white";'
+   })
+  console.log('waka flaka')
   // No tabs or host permissions needed!
   // chrome.tabs.executeScript({
   //   code: 'document.body.style.backgroundColor="red"'
