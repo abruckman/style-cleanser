514502214429b44c5ead5088c2e14734eaae1f7a
abruckman <abruckman09@gmail.com>
2017-01-31 12:29:46 -0800

    icon and functionality restored

--- color-change.js
+++ color-change.js
@@ -1,7 +1,8 @@
 
-console.log('selection');
+
 var target = window.getSelection().anchorNode.parentElement;
-console.log(target);
+
 // .stlye.cssText = "background-color: white";
-target.parentElement.style.cssText += "background-color: white";
+// target.parentElement.style.cssText += "background-color: white";
 target.style.cssText += "color: black"
+target.style.cssText += "background-color: white"
--- content.js
+++ content.js
@@ -3,7 +3,7 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript({
     file: 'color-change.js',
   //   code: 'window.getSelection().anchorNode.parentElement.stlye.cssText = "background-color: white";'
-   })
+  })
   console.log('waka flaka')
   // No tabs or host permissions needed!
   // chrome.tabs.executeScript({
